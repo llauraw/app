fb6ea5fc54a17f3ae496ad20995dbf2446cce5d3
llauraw <121110381+llauraw@users.noreply.github.com>
2022-12-20 16:00:12 +0100

    app

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     const query = require('url').parse(req.url, true).query;
-    const n1 = parseInt(query.n1, 10);
-    const n2 = parseInt(query.n2, 10);
+    const n1 = parseInt(query.n1, 10) || 4;
+    const n2 = parseInt(query.n2, 10) || 10;
     sum = n1+n2;
     res.write(n1+'+'+n2+'='+sum);
     console.log(n1+'+'+n2+'='+sum);
