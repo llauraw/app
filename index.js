9ad1831a6b76a4073bc4790d39e8097ac02f55fe
llauraw <121110381+llauraw@users.noreply.github.com>
2022-12-20 15:54:12 +0100

    app

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     n1 = 5;
     n2 = 10;
-    res.write(n1+'+'+n2+'='+(n1+n2));
+    sum = n1+n2;
+    res.write(n1+'+'+n2+'='+sum);
+    console.log(n1+'+'+n2+'='+sum);
     res.end();
 }).listen(process.env.PORT || 3000);
