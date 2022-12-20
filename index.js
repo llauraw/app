0699a214cb582b01cf475ed3da25190fc94646d2
llauraw <121110381+llauraw@users.noreply.github.com>
2022-12-20 15:48:26 +0100

    app

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    n1 = 5;
+    n2 = 10;
+    res.write(n1+'+'+n2+'='+n1+n2);
     res.end();
 }).listen(process.env.PORT || 3000);
